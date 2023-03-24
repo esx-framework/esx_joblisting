e07f69f518bbb6f8fa466689e9cdfcfce622544e
C3h1c <fivemsloofficial@gmail.com>
2023-03-24 19:42:46 +0100

    Update si.lua
    This is sucks, before my translate... xD

--- locales/si.lua
+++ locales/si.lua
@@ -1,5 +1,5 @@
 Locales['si'] = {
-  ['new_job'] = 'imaš novo službo!',
-  ['access_job_center'] = 'pritisnite ~INPUT_PICKUP~ za dostop do job center.',
-  ['job_center'] = 'center za delovna mesta',
+  ['new_job'] = 'Imate novo Službo!',
+  ['access_job_center'] = 'Pritisni ~INPUT_PICKUP~ za dostop do Borze.',
+  ['job_center'] = 'Center za Zaposlovanje',
 }
