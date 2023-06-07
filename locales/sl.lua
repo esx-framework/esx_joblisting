f5244d5a7404daef5e917f02e3e1d4d5cbf0880d
C3h1c <fivemsloofficial@gmail.com>
2023-06-07 17:55:10 +0200

    sl.lua

---
+++ locales/sl.lua
@@ -0,0 +1,6 @@
+Locales['sl'] = {
+  ['new_job'] = 'Nova Sluzba: ~b~%s~s~ !',
+  ['access_job_center'] = 'Pritisni ~b~[E]~s~ da odpres zaposlovalnico.',
+  ['job_center'] = 'Izberite posel.',
+  ["blip_text"] = "ZZS"
+}
