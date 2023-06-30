e39a2adc6e21d6b69c9fc35945d6177ec04827e4
C3h1cDevelopment <128829862+C3-FrameWork@users.noreply.github.com>
2023-06-30 17:47:34 +0200

    sl.lua

--- locales/sl.lua
+++ locales/sl.lua
@@ -1,6 +1,8 @@
 Locales['sl'] = {
-  ['new_job'] = 'Nova Sluzba: ~b~%s~s~ !',
-  ['access_job_center'] = 'Pritisni ~b~[E]~s~ da odpres zaposlovalnico.',
-  ['job_center'] = 'Izberite posel.',
-  ["blip_text"] = "ZZS"
+  ['new_job'] = 'imaš novo službo!',
+  ['access_job_center'] = 'pritisnite ~INPUT_PICKUP~ za dostop do job center.',
+  ['job_center'] = 'center za delovna mesta',
+  ['new_job'] = 'Imate novo Službo!',
+  ['access_job_center'] = 'Pritisni ~INPUT_PICKUP~ za dostop do Borze.',
+  ['job_center'] = 'Center za Zaposlovanje',
 }
