7a7e8cfde352889bb176bdd7b190bfe580b3d547
Kasey FItton <kaseyfitton@hotmail.co.uk>
2024-11-09 09:51:25 +0000

    refactor: Backend rewrite

--- client/main.lua
+++ client/main.lua
@@ -1,88 +1,154 @@
-local menuIsShowed, isNear, TextUIdrawing = false, false, false
+local JobCentre = {}
 
-function ShowJobListingMenu()
-  menuIsShowed = true
+JobCentre.isNear = false
+JobCentre.menuOpen = false
+JobCentre.TextUIActive = false
+
+function JobCentre:GetJobs()
+  local promise = promise:new()
+
   ESX.TriggerServerCallback('esx_joblisting:getJobsList', function(jobs)
-    local elements = {{unselectable = "true", title = TranslateCap('job_center'), icon = "fas fa-briefcase"}}
+    promise:resolve(jobs)
+  end)
 
-    for i = 1, #(jobs) do
-      elements[#elements + 1] = {title = jobs[i].label, name = jobs[i].name}
+  Citizen.Await(promise)
+  return promise.value
+end
+
+function JobCentre:InsertJobs()
+  local jobs = self:GetJobs()
+  local elements = {}
+  local unemployed = {}
+
+  for i=1, #jobs, 1 do
+    local job = jobs[i]
+    if job.name ~= 'unemployed' then
+      elements[#elements + 1] = {
+        label = job.label,
+        value = job.name
+      }
+    else
+      unemployed = {
+        label = job.label,
+        value = job.name
+      }
     end
+  end
 
-    ESX.OpenContext("right", elements, function(menu, SelectJob)
-      TriggerServerEvent('esx_joblisting:setJob', SelectJob.name)
-      ESX.CloseContext()
-      ESX.ShowNotification(TranslateCap('new_job', SelectJob.title), "success")
-      menuIsShowed = false
-      TextUIdrawing = false
-    end, function()
-      menuIsShowed = false
-      TextUIdrawing = false
-    end)
+  table.sort(elements, function(a, b)
+    return a.label < b.label
+  end)
+
+  table.insert(elements, 1, unemployed)
+
+  return elements
+end
+
+function JobCentre:OnSelect(value)
+  ESX.TriggerServerCallback('esx_joblisting:setJob', function(isJob)
+    if not isJob then
+      ESX.ShowNotification('Unable to employ you as a '.. value, "error")
+      return
+    end
+    ESX.ShowNotification('You are now employed as a '.. value, "success")
+  end, value)
+end
+
+function JobCentre:Close(menu)
+  menu.close()
+  self.menuOpen = false
+end
+
+function JobCentre:ShowMenu()
+  self.menuOpen = true
+
+  local elements = self:InsertJobs()
+
+  ESX.UI.Menu.Open('default', GetCurrentResourceName(), 'joblisting', {
+    title = 'Job Centre',
+    align = 'top-left',
+    elements = elements
+  }, function(data, menu)
+    self:OnSelect(data.current.value)
+    self:Close(menu)
+  end, function(_, menu)
+    self:Close(menu)
   end)
 end
 
--- Activate menu when player is inside marker, and draw markers
+function JobCentre:CreateBlip(coords)
+  local blip = AddBlipForCoord(coords.x, coords.y, coords.z)
+
+  SetBlipSprite(blip, Config.Blip.Sprite)
+  SetBlipDisplay(blip, Config.Blip.Display)
+  SetBlipScale(blip, Config.Blip.Scale)
+  SetBlipColour(blip, Config.Blip.Colour)
+  SetBlipAsShortRange(blip, Config.Blip.ShortRange)
+
+  BeginTextCommandSetBlipName("STRING")
+  AddTextComponentSubstringPlayerName(TranslateCap('blip_text'))
+  EndTextCommandSetBlipName(blip)
+end
+
+function JobCentre:TextUI(text)
+  ESX.TextUI(text)
+  self.TextUIActive = true
+end
+
+function JobCentre:HideTextUI()
+  ESX.HideUI()
+  self.TextUIActive = false
+end
+
+function JobCentre:ToggleTextUI()
+  if self.isNear and not self.TextUIActive then
+    self:TextUI(TranslateCap('access_job_center', ESX.GetInteractKey()))
+  else
+    if not self.isNear and self.TextUIActive then
+      self:HideTextUI()
+    end
+  end
+end
+
+function JobCentre:InsidePoint(distance, coords)
+  self.isNear = distance < (Config.ZoneSize.x / 2)
+
+  DrawMarker(Config.MarkerType, coords.x, coords.y, coords.z, 0.0, 0.0, 0.0, 0, 0.0, 0.0, Config.ZoneSize.x, Config.ZoneSize.y, Config.ZoneSize.z,
+  Config.MarkerColor.r, Config.MarkerColor.g, Config.MarkerColor.b, 200, false, true, 2, false, false, false, false)
+
+  self:ToggleTextUI()
+end
+
+function JobCentre:LeavePoint()
+  self.isNear = false
+  self:HideTextUI()
+end
+
+function JobCentre:CreatePoint(coords)
+  ESX.Point:new({
+    coords = coords,
+    distance = Config.DrawDistance,
+    inside = function(point)
+      self:InsidePoint(point.currDistance, point.coords)
+    end,
+    leave = function()
+      self:LeavePoint()
+    end
+  })
+end
+
 CreateThread(function()
-  while true do
-    local Sleep = 1000
-
-    local coords = GetEntityCoords(ESX.PlayerData.ped)
-    local isInMarker = false
-
-    for i = 1, #Config.Zones, 1 do
-      local distance = #(coords - Config.Zones[i])
-
-      if distance < Config.DrawDistance then
-        Sleep = 0
-        DrawMarker(Config.MarkerType, Config.Zones[i].x, Config.Zones[i].y, Config.Zones[i].z, 0.0, 0.0, 0.0, 0, 0.0, 0.0, Config.ZoneSize.x, Config.ZoneSize.y, Config.ZoneSize.z,
-        Config.MarkerColor.r, Config.MarkerColor.g, Config.MarkerColor.b, 100, false, true, 2, false, false, false, false)
-
-        isNear = distance < (Config.ZoneSize.x / 2)
-        isInMarker = true
-
-        if isNear and not TextUIdrawing then
-          ESX.TextUI(TranslateCap('access_job_center', ESX.GetInteractKey()))
-          TextUIdrawing = true
-        else
-          if not isNear and TextUIdrawing then
-            ESX.HideUI()
-            TextUIdrawing = false
-          end
-        end
-      end
+  for i=1, #Config.Zones, 1 do
+    local coords = Config.Zones[i]
+    if Config.Blip.Enabled then
+      JobCentre:CreateBlip(coords)
     end
-
-    if not isInMarker and TextUIdrawing then
-      ESX.HideUI()
-      TextUIdrawing = false
-    end
-
-    Wait(Sleep)
+    JobCentre:CreatePoint(coords)
   end
 end)
 
--- Create blips
-if Config.Blip.Enabled then
-  CreateThread(function()
-    for i = 1, #Config.Zones, 1 do
-      local blip = AddBlipForCoord(Config.Zones[i])
-
-      SetBlipSprite(blip, Config.Blip.Sprite)
-      SetBlipDisplay(blip, Config.Blip.Display)
-      SetBlipScale(blip, Config.Blip.Scale)
-      SetBlipColour(blip, Config.Blip.Colour)
-      SetBlipAsShortRange(blip, Config.Blip.ShortRange)
-
-      BeginTextCommandSetBlipName("STRING")
-      AddTextComponentSubstringPlayerName(TranslateCap('blip_text'))
-      EndTextCommandSetBlipName(blip)
-    end
-  end)
-end
-
 ESX.RegisterInteraction("open_joblisting", function()
-  ShowJobListingMenu()
+  JobCentre:ShowMenu()
 end, function()
-  return isNear and not menuIsShowed
+  return JobCentre.isNear and not JobCentre.menuOpen
 end)
--- server/main.lua
+++ server/main.lua
@@ -1,55 +1,66 @@
-function getJobs()
+local Server = {}
+
+function Server:GetJobs()
   local jobs = ESX.GetJobs()
   local availableJobs = {}
+
   for k, v in pairs(jobs) do
     if v.whitelisted == false then
       availableJobs[#availableJobs + 1] = {label = v.label, name = k}
     end
   end
+
   return availableJobs
 end
 
-ESX.RegisterServerCallback('esx_joblisting:getJobsList', function(source, cb)
-  local jobs = getJobs()
-  cb(jobs)
-end)
-
-function IsJobAvailable(job)
+function Server:IsJobAvailable(job)
   local jobs = ESX.GetJobs()
   local JobToCheck = jobs[job]
+
   return not JobToCheck.whitelisted
 end
 
-function IsNearCentre(player)
+function Server:IsNearCentre(player)
   local Ped = GetPlayerPed(player)
   local PedCoords = GetEntityCoords(Ped)
-  local Zones = Config.Zones
-  local Close = false
 
-  for i = 1, #Config.Zones, 1 do
-    local distance = #(PedCoords - Config.Zones[i])
+  local Zones = Config.Zones
+
+  for i = 1, #Zones, 1 do
+    local distance = #(PedCoords - Zones[i])
 
     if distance < Config.DrawDistance then
-      Close = true
+      return true
     end
   end
 
-  return Close
+  return false
 end
 
-RegisterServerEvent('esx_joblisting:setJob')
-AddEventHandler('esx_joblisting:setJob', function(job)
-  local source = source
+ESX.RegisterServerCallback('esx_joblisting:setJob', function(source, cb, job)
   local xPlayer = ESX.GetPlayerFromId(source)
-  local jobs = getJobs()
+  local IsNearCentre = Server:IsNearCentre(source)
+  local IsJobAvailable = Server:IsJobAvailable(job)
 
-  if xPlayer and IsNearCentre(source) and IsJobAvailable(job) then
+  if xPlayer and IsNearCentre and IsJobAvailable then
     if ESX.DoesJobExist(job, 0) then
       xPlayer.setJob(job, 0)
+      cb(true)
+      return
     else
       print("[^1ERROR^7] Tried Setting User ^5".. source .. "^7 To Invalid Job - ^5"..job .."^7!")
+      cb(false)
+      return
     end
-  else 
+  else
     print("[^3WARNING^7] User ^5".. source .. "^7 Attempted to Exploit ^5`esx_joblisting:setJob`^7!")
+    cb(false)
+    return
   end
+  cb(true)
+end)
+
+ESX.RegisterServerCallback('esx_joblisting:getJobsList', function(source, cb)
+  local jobs = Server:GetJobs()
+  cb(jobs)
 end)
